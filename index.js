6cfd8e22337372d8f1a7ab76a97ef598fe51b0a3
AakashAckerman <aakash.ackerman@gmail.com>
2025-05-27 15:05:32 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 console.log("🕵️‍♂️ Looking under the hood? Respect.");
+
+const togglebtn = document.getElementById("mode-toggle")
+const docBody = document.body
+
+function modeToggle(){
+    if(docBody.className == "dark-mode"){
+        docBody.classList.remove("dark-mode")
+        docBody.classList.add("light-mode")
+        togglebtn.textContent = "☀️"
+    }else{
+        docBody.classList.remove("light-mode")
+        docBody.classList.add("dark-mode")
+        togglebtn.textContent = "🌙"
+    }
+}
